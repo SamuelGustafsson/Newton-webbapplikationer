e7c74eeb224b0234bf3cbaf653a648290ed97d26
Samuel Gustafsson <Samuel Gustafsson>
2016-06-04 12:41:04 +0200

    Form validation

--- js/contact-form.js
+++ js/contact-form.js
@@ -3,15 +3,42 @@ var elContactName = document.getElementById('contact-name');
 var elContactEmail = document.getElementById('contact-email');
 var elContactPhone = document.getElementById('contact-phone');
 var elContactMessage = document.getElementById('contact-form-message');
-console.log('Hejsan');
+
+
+
+
+function emailValidation (email) {
+      var reg = /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z0-9_\-\.]){1,}\.([A-Za-z]{2,4})$/;
+      if (reg.test(email.value) == false) {
+            return false
+      }
+      
+}
+
 function checkForm(event) {
       event.preventDefault();
-   console.log('i funktionen');
    if (elContactName.value.length < 2) {
-      elContactMessage.textContent = 'Fyll i ditt användarnamn!';
+      elContactMessage.textContent = 'Invalid username';
    }
+   
+   else if (emailValidation(elContactEmail) == false) {
+         alert('Invalid email, example "person@company.com"');
+   }
+   
+   else if (isNaN(elContactPhone.value) || elContactPhone.value == null) {
+         elContactMessage.textContent = 'Your phonenumber cant have letters';
+   }
+   else if (elContactPhone.value.length < 10) {
+         elContactMessage.textContent = 'Phonenumber is invalid. Write you number with 10 numbers.'
+   }
+
+   
    else {
-      elContactMessage.textContent = '';
+         
+          if (elContactMessage.value == null){
+            elContactMessage.value = "";
+            }
+      alert("Message has been sent! \nFrom: " + elContactName.value + "\nEmail:  " + elContactEmail.value + "\nPhone: " + elContactPhone.value + "\nMessage: " + elContactMessage.value);
    }
 
 }
