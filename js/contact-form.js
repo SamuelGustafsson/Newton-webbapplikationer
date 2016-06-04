3d34080aa93cfcfaf4f35789133e85a983745695
Samuel Gustafsson <Samuel Gustafsson>
2016-06-04 13:58:28 +0200

    Javascript validering

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var elContactForm = document.getElementById('contact-form');
 var elContactName = document.getElementById('contact-name');
 var elContactEmail = document.getElementById('contact-email');
@@ -5,14 +7,11 @@ var elContactPhone = document.getElementById('contact-phone');
 var elContactMessage = document.getElementById('contact-form-message');
 var elContactTextArea = document.getElementById('contact-textarea');
 
-
-
 function emailValidation (email) {
       var reg = /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z0-9_\-\.]){1,}\.([A-Za-z]{2,4})$/;
       if (reg.test(email.value) == false) {
-            return false
+            return false;
       }
-      
 }
 
 function checkForm(event) {
@@ -20,11 +19,9 @@ function checkForm(event) {
    if (elContactName.value.length < 2) {
       elContactMessage.textContent = 'Invalid username';
    }
-   
    else if (emailValidation(elContactEmail) == false) {
          alert('Invalid email, example "person@company.com"');
    }
-   
    else if (isNaN(elContactPhone.value) || elContactPhone.value == null) {
          elContactMessage.textContent = 'Your phonenumber cant have letters';
    }
@@ -34,11 +31,8 @@ function checkForm(event) {
    else if (elContactTextArea == null){
          elContactTextArea.textContent = '';
    }
-      
    else {
       alert("Message has been sent! \nFrom: " + elContactName.value + "\nEmail:  " + elContactEmail.value + "\nPhone: " + elContactPhone.value + "\nMessage: " + elContactTextArea.value);
    }
-
 }
-
 elContactForm.addEventListener('submit', checkForm, false);
